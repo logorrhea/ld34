85f429cb7eb67eace1de6607ce6b2569c95b880d
Tyler Funk <tfunk@fastmail.fm>
2015-12-11 20:50:46 -0600

    In the beginning, there was only darkness

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject head;
+    public float growRate;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        MovePlayer();
+    }
+
+    void MovePlayer() {
+        Vector3 pos = head.transform.position;
+        pos.y += growRate;
+        head.transform.position = pos;
+    }
+}
