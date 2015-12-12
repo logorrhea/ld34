8079e0ad086eba9b8271f9cad5b55b33bd3e717f
Tyler Funk <tfunk@fastmail.fm>
2015-12-11 21:40:24 -0600

    Basic player movement, camera following

--- Assets/MainCamera.cs
+++ Assets/MainCamera.cs
@@ -4,15 +4,25 @@ using System.Collections;
 
 public class MainCamera : MonoBehaviour {
 
-    public GameObject player;
+    GameObject Player;
+    bool isFollowingPlayer = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (isFollowingPlayer) {
+            Vector3 pos = this.transform.position;
+            this.transform.position = new Vector3(pos.x, Player.transform.position.y, pos.z);
+        }
+    }
 
-	}
+    void StartFollowPlayer(GameObject player) {
+        Player = player;
+        isFollowingPlayer = true;
+    }
+
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,11 +4,22 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
 
-    public GameObject head;
-    public float growRate;
+    public float CameraLockTrigger = 0.4f;
+    public GameObject Head;
+    public float GrowRate;
+
+    float AxisMin = 0.2f;
+
+    bool isCameraLocked = false;
 
     // Use this for initialization
     void Start () {
+    }
+
+    void Update() {
+        if (!isCameraLocked) {
+            TryLockCamera();
+        }
     }
 
     // Update is called once per frame
@@ -17,8 +28,16 @@ public class PlayerController : MonoBehaviour {
     }
 
     void MovePlayer() {
-        Vector3 pos = head.transform.position;
-        pos.y += growRate;
-        head.transform.position = pos;
+        Vector3 pos = Head.transform.position;
+        Vector3 moveDirection = Vector3.up;
+        moveDirection.x = Input.GetAxis("Horizontal");
+        Head.transform.position = Vector3.Lerp(pos, pos + moveDirection, GrowRate);
+    }
+
+    void TryLockCamera() {
+        if (Head.transform.position.y >= CameraLockTrigger) {
+            isCameraLocked = true;
+            Camera.main.SendMessage("StartFollowPlayer", Head);
+        }
     }
 }
