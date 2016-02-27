a9a42a557bd4a4b19fce1ff08b9ea4c58dd989c4
Tyler Funk <tfunk@fastmail.fm>
2016-02-27 12:39:38 -0600

    Difficulty curve tweaks
    The difficulty of the game now slowly increases over time by:
    1) Increasing the rate at which new crows spawn
    2) Increasing the maximum number of enemies which can exist at a time

--- Assets/ObstacleSpawner.cs
+++ Assets/ObstacleSpawner.cs
@@ -8,6 +8,10 @@ public class ObstacleSpawner : MonoBehaviour
     public Transform SpawnPoint;
     public float SpawnRate;
 
+    public int EnemyCap;
+    public float EnemyCapIncreaseRate;
+    float TimeSinceLastCapIncrease = 0.0f;
+
     List<GameObject> SpawnedUnits;
 
     float LeftBounds, RightBounds;
@@ -19,6 +23,18 @@ public class ObstacleSpawner : MonoBehaviour
         Bounds bounds = CameraExtensions.OrthographicBounds(Camera.main);
         LeftBounds = bounds.min.x;
         RightBounds = bounds.max.x;
+    }
+
+    void Update() {
+        // Increase number of potential units on screen over time
+        TimeSinceLastCapIncrease += Time.deltaTime;
+        if (TimeSinceLastCapIncrease >= EnemyCapIncreaseRate && !GameState.isPaused)
+        {
+            TimeSinceLastCapIncrease = 0.0f;
+            Debug.Log("feel the bern");
+            SpawnRate -= 0.05f;
+            EnemyCap++;
+        }
     }
 
     public void StartSpawningUnits()
@@ -47,11 +63,14 @@ public class ObstacleSpawner : MonoBehaviour
 
     private float SpawnUnit()
     {
-        GameObject unit = Units[Random.Range(0, Units.Length)];
-        Vector3 spawnPoint = SpawnPoint.position;
-        spawnPoint.x = Random.Range(LeftBounds, RightBounds);
-        GameObject newUnit = (GameObject)Instantiate(unit, spawnPoint, Quaternion.identity);
-        SpawnedUnits.Add(newUnit);
+        if (Units.Length < EnemyCap)
+        {
+            GameObject unit = Units[Random.Range(0, Units.Length)];
+            Vector3 spawnPoint = SpawnPoint.position;
+            spawnPoint.x = Random.Range(LeftBounds, RightBounds);
+            GameObject newUnit = (GameObject)Instantiate(unit, spawnPoint, Quaternion.identity);
+            SpawnedUnits.Add(newUnit);
+        }
         return SpawnRate;
     }
 }
